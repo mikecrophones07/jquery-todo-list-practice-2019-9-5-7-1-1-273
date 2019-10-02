c7b121a491e405782f36c6e883798182d2cf9a0c
Mike Anthony A. Echon <mike.anthony.echon@oocl.com>
2019-10-02 11:40:54 +0800

    edit todo.js logic to attain objectives of the activity

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,6 @@
+
 $(document)
     .ready(function () {
-
         function generateUUID() {
             /*jshint bitwise:false */
             var i,
@@ -21,5 +21,66 @@ $(document)
             return uuid;
         }
 
+        function appendItems(){
+            var str1 = '<li id="' + generateUUID() +'" class=""> <input name="done-todo" type="checkbox" class="done-todo"><span>' + $('.input-text').val() + '</span></li>';
+            $('ol').append(str1);
+        }
+
+        function updateViewListCheckBox(element){
+            var listId = '#' + $(element.parentElement).attr('id');
+
+            if($(listId).hasClass('checked')){
+                $(listId).removeClass();
+            }else{
+                $(listId).attr('class','checked');
+            }
+        }
+
         // code to be implemented
+
+        $('#button').click(function(){
+            appendItems();
+        });
+
+        $(document).on('click', '.done-todo' , function() {
+            updateViewListCheckBox(this);
+        });
+
+        $(document).on('dblclick', 'li' , function(e) {
+            $(this).children('span').attr('contenteditable', 'true').focus();
+            $(this).keypress(function(e){
+                if(e.keyCode == 13){
+                    // $(this).html($(this).text());
+                    $(this).children('span').attr('contenteditable', 'false').focus();
+                }
+            });
+        });
+
+        $('#filters li a').on('click', function(){
+            var condition = this.text;
+            $('ol li').each(function(){
+                var listElements = this;
+                if(condition == "ALL"){
+                    $(listElements).show();
+                }
+                else if (condition == "Active"){
+                    if($(listElements).hasClass('checked')){
+                        $(listElements).hide();
+                    }
+                    else{
+                        $(listElements).show();
+                    }
+                }
+                else{
+                    if(!$(listElements).hasClass('checked')){
+                        $(listElements).hide();
+                    }
+                    else{
+                        $(listElements).show();
+                    }
+                }
+            });
+        });
     });
+
+    
